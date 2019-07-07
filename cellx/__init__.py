34ece5928b5948f6c5371270cae9df38d66131bd
Hiroyuki Ohsaki <ohsaki@lsnl.jp>
2019-07-07 21:47:13 +0900

    *** empty log message ***

--- cellx/__init__.py
+++ cellx/__init__.py
@@ -1,6 +1,5 @@
-from cellx.cellx import *
-from cellx.object import *
-from cellx.parser import *
-from cellx.util import *
-
-from cellx.monitor import *
+from .cell import *
+from .monitor import *
+from .object import *
+from .parser import *
+from .util import *
