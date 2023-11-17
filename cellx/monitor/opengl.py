ad5a2e6b02ff1646b142af4c50e8e784d0f50b1a
Hiroyuki Ohsaki <ohsaki@lsnl.jp>
2023-11-17 20:51:31 +0900

    *** empty log message ***

--- cellx/monitor/opengl.py
+++ cellx/monitor/opengl.py
@@ -30,6 +30,8 @@ import cellx
 import pygame
 from cellx.monitor.sdl import SDL
 
+import tbdump
+
 class OpenGL(SDL):
     def __init__(self, *kargs, **kwargs):
         super().__init__(*kargs, **kwargs)
@@ -48,6 +50,7 @@ class OpenGL(SDL):
         gl.glMatrixMode(gl.GL_MODELVIEW)
 
         gl.glShadeModel(gl.GL_SMOOTH)
+        # FIXME: Not supported?
         # gl.glShadeModel(gl.GL_LINE_SMOOTH)
 
         gl.glPixelStorei(gl.GL_UNPACK_ALIGNMENT, 1)
@@ -66,19 +69,15 @@ class OpenGL(SDL):
 
         # Configure lights.
         mat_specular = (1, 1, 0, 1)
+        gl.glMaterial(gl.GL_FRONT, gl.GL_SPECULAR, *mat_specular)
         mat_diffuse = (0, 1, 1, 1)
-        light_position = (-.25, .25, .5, 0)
-        gl.glMaterialfv(gl.GL_FRONT, gl.GL_SPECULAR,
-                        struct.pack('4f', *mat_specular))
-        gl.glMaterialfv(gl.GL_FRONT, gl.GL_DIFFUSE,
-                        struct.pack('4f', *mat_diffuse))
+        gl.glMaterial(gl.GL_FRONT, gl.GL_DIFFUSE, *mat_diffuse)
         gl.glMaterialf(gl.GL_FRONT, gl.GL_SHININESS, 50)
-        gl.glLightfv(gl.GL_LIGHT0, gl.GL_POSITION,
-                     struct.pack('4f', *light_position))
-        gl.glLightfv(gl.GL_LIGHT0, gl.GL_AMBIENT,
-                     struct.pack('4f', .3, .3, .3, 1))
-        gl.glLightModelfv(gl.GL_LIGHT_MODEL_AMBIENT,
-                          struct.pack('4f', .3, .3, .3, 1))
+        light_position = (-.25, .25, .5, 0)
+        gl.glLight(gl.GL_LIGHT0, gl.GL_POSITION, *light_position)
+        gl.glLight(gl.GL_LIGHT0, gl.GL_AMBIENT, (.3, .3, .3, 1))
+        # FIXME: Not supported?
+        # gl.glLightModel(gl.GL_LIGHT_MODEL_AMBIENT, (.3, .3, .3, 1))
 
     def normalize_position(self, x, y):
         return (x - self.width / 2) / self.width, (
@@ -86,6 +85,17 @@ class OpenGL(SDL):
 
     def normalize_size(self, l):
         return l / self.width
+
+    def draw_line(self, sx, sy, dx, dy, width, color, alpha):
+        sx, sy = self.normalize_position(sx, sy)
+        dx, dy = self.normalize_position(dx, dy)
+        color = self.palette.rgba(color, alpha)
+        gl.glLineWidth(width)
+        gl.glColor4ub(*color)
+        gl.glBegin(gl.GL_LINES)
+        gl.glVertex2d(sx, sy)
+        gl.glVertex2d(dx, dy)
+        gl.glEnd()
 
     def _render_box(self, obj):
         x, y = self.normalize_position(obj.x, obj.y)
@@ -95,11 +105,9 @@ class OpenGL(SDL):
         gl.glPushMatrix()
         gl.glTranslatef(x, y, obj.priority)
         gl.glScalef(w, h, (w + h) / 2)
-        gl.glMaterialfv(gl.GL_FRONT, gl.GL_EMISSION,
-                        struct.pack('4f', .2, .2, 0, 1))
+        gl.glMaterial(gl.GL_FRONT, gl.GL_EMISSION, (.2, .2, 0, 1))
         glut.glutSolidCube(1)
-        gl.glMaterialfv(gl.GL_FRONT, gl.GL_EMISSION,
-                        struct.pack('4f', 0, 0, 0, 1))
+        gl.glMaterial(gl.GL_FRONT, gl.GL_EMISSION, (0, 0, 0, 1))
         gl.glPopMatrix()
 
     def _render_ellipse(self, obj):
@@ -112,11 +120,31 @@ class OpenGL(SDL):
         glut.glutSolidSphere(r, 20, 20)
         gl.glPopMatrix()
 
-        pygame.display.flip()
+    def render_objects(self, objs):
+        # FIXME: support `fix' objects.
+        super().render_objects(objs)
 
-        pygame.display.flip()
-        import time
-        time.sleep(10)
+    def _clear(self):
+        gl.glClear(gl.GL_COLOR_BUFFER_BIT | gl.GL_DEPTH_BUFFER_BIT)
+
+        xoffset, yoffset = 0, 0
+        zoom = .7
+        gl.glMatrixMode(gl.GL_PROJECTION)
+        # gl.glLoadIdentity();
+        # gl.glOrtho(
+        #     xoffset - .5 * zoom,
+        #         xoffset + .5 * zoom,
+        #             yoffset - .5 * zoom,
+        #                 yoffset + .5 * zoom,
+        #                     -100, 100
+        #                         )
+
+        gl.glMatrixMode(gl.GL_MODELVIEW);
+        # gl.glLoadIdentity();
+
 
     def _display(self):
         pygame.display.flip()
+
+
+        gl.glRotatef(.01, .01, .02, 1)
