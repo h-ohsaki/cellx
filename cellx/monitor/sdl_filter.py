cce75c1b24155921aa9d421992788e10efe09882
Hiroyuki Ohsaki <ohsaki@lsnl.jp>
2023-11-19 00:02:56 +0900

    update comments

--- cellx/monitor/sdl_filter.py
+++ cellx/monitor/sdl_filter.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python3
 #
 #
-# Copyright (c) 2018-2019, Hiroyuki Ohsaki.
+# Copyright (c) 2018-2023, Hiroyuki Ohsaki.
 # All rights reserved.
-#
-# $Id: sdl_filter.py,v 1.2 2019/03/10 11:50:05 ohsaki Exp ohsaki $
 #
 
 # This program is free software: you can redistribute it and/or modify
@@ -20,12 +18,10 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-import math
 import pygame
 import pygame.gfxdraw
 import pygame.transform
 
-import cellx
 from cellx.monitor.sdl import SDL
 
 class SDL_Filter(SDL):
@@ -38,17 +34,17 @@ class SDL_Filter(SDL):
         self.current_frame.set_alpha(self.alpha)
 
     def render_objects(self, objs):
-        # render fixed but not-rendered objects
+        # Render fixed but not-rendered objects.
         self.screen = self.fixed_surface
         for obj in objs:
             if obj.fixed and not self.rendered.get(obj, None):
                 self.render(obj)
                 self.rendered[obj] = True
 
-        # reset to pre-rendered suface
+        # Reset to pre-rendered suface.
         self.current_frame.blit(self.fixed_surface, (0, 0))
 
-        # render non-fixed objects
+        # Render non-fixed objects.
         self.screen = self.current_frame
         for obj in objs:
             if not obj.fixed:
