256e7e1c4e4745d02fb561ac4fb1c5083d0d9905
Hiroyuki Ohsaki <ohsaki@lsnl.jp>
2023-11-18 10:41:51 +0900

    *** empty log message ***

--- cellx/monitor/opengl.py
+++ cellx/monitor/opengl.py
@@ -44,6 +44,7 @@ class OpenGL(SDL):
         self.rot_z = 0.
         self.last_button = None
         super().__init__(*kargs, **kwargs)
+        self.font_bitmap = self.load_font()
 
     def init(self):
         super().init()
@@ -88,6 +89,15 @@ class OpenGL(SDL):
     def relative_position(self, x, y):
         return x / self.width, y / self.height
 
+    def load_font(self):
+        BITMAP_FONT = '/home/ohsaki/lib/fonts/8x8maru.fnt'
+        with open(BITMAP_FONT, 'rb') as f:
+            font_data = f.read()
+        # Split font data per 8 bytes.
+        # FIXME: should not hard-code font size
+        # FIXME: support Kanji characters
+        return [font_data[i:i + 8][::-1] for i in range(0, len(font_data), 8)]
+
     def draw_line(self, sx, sy, dx, dy, width, color, alpha, use_line=False):
         if sx > dx:
             sx, sy, dx, dy = dx, dy, sx, sy
@@ -103,20 +113,37 @@ class OpenGL(SDL):
             gl.glVertex2d(sx, sy)
             gl.glVertex2d(dx, dy)
             gl.glEnd()
-        else:
-            # Place a square rod connecting (SX, SY) and (DX, DY).
-            # This code assumes SX is smaller than DX.
-            gl.glPushMatrix()
-            gl.glTranslatef(sx, sy, 0)
-            angle = math.degrees(math.atan2(dy - sy, dx - sx))
-            gl.glRotatef(angle, 0, 0, 1)
-            l = math.sqrt((dx - sx)**2 + (dy - sy)**2)
-            # Randomly rotate for better visibility.
-            gl.glRotatef(180 * sx, 1, 0, 0)
-            gl.glScalef(l, w, w)
-            gl.glTranslatef(.5, 0, 0)
-            glut.glutSolidCube(1)
-            gl.glPopMatrix()
+            return
+
+        # Place a square rod connecting (SX, SY) and (DX, DY).
+        # This code assumes SX is smaller than DX.
+        gl.glPushMatrix()
+        gl.glTranslatef(sx, sy, 0)
+        angle = math.degrees(math.atan2(dy - sy, dx - sx))
+        gl.glRotatef(angle, 0, 0, 1)
+        l = math.sqrt((dx - sx)**2 + (dy - sy)**2)
+        # Randomly rotate for better visibility.
+        # gl.glRotatef(180 * sx, 1, 0, 0)
+        gl.glScalef(l, w, w)
+        gl.glTranslatef(.5, 0, 0)
+        glut.glutSolidCube(1)
+        gl.glPopMatrix()
+
+    def draw_ellipse(self, x, y, r, color, alpha):
+        x, y = self.normalize_position(x, y)
+        r = self.normalize_size(r)
+        color = self.palette.rgba(color, alpha)
+        gl.glColor4ub(*color)
+        gl.glBegin(gl.GL_POLYGON)
+        # Draw the ellipse with 20 segments.
+        for degree in range(0, 360, 360 // 20):
+            radians = math.radians(degree)
+            x1, y1 = x + r * math.cos(radians), y + r * math.sin(radians)
+            gl.glVertex2d(x1, y1)
+        gl.glEnd()
+
+    def _render_bitmap(self, obj):
+        raise NotImplementedError
 
     def _render_box(self, obj):
         x, y = self.normalize_position(obj.x, obj.y)
@@ -130,7 +157,11 @@ class OpenGL(SDL):
         glut.glutSolidCube(1)
         gl.glPopMatrix()
 
-    def _render_ellipse(self, obj):
+    def _render_ellipse(self, obj, use_polygon=False):
+        if use_polygon:
+            self.draw_ellipse(obj.x, obj.y, obj.width / 2, obj.color, obj.alpha)
+            return
+
         x, y = self.normalize_position(obj.x, obj.y)
         z = self.normalize_size(obj.priority)
         r = self.normalize_size(obj.width / 2)
@@ -139,6 +170,45 @@ class OpenGL(SDL):
         gl.glPushMatrix()
         gl.glTranslatef(x, y, z)
         glut.glutSolidSphere(r, 20, 20)
+        gl.glPopMatrix()
+
+
+    def _render_polygon( self, obj ):
+        vertices = obj.vertices()
+        color = self.palette.rgba(obj.color, obj.alpha)
+        gl.glColor4ub( *color)
+        gl.glBegin(gl.GL_POLYGON)
+        for v in vertices:
+            x, y  = self.normalize_position( *v )
+            gl.glVertex2d( x, y )
+        gl.glEnd()
+
+    def _render_spline(self, obj):
+        x, y = self.normalize_position(obj.x, obj.y)
+        x2, y2 = self.normalize_position(obj.x2, obj.y2)
+        x3, y3 = self.normalize_position(obj.x3, obj.y3)
+        w = self.normalize_size(obj.width)
+        color = self.palette.rgba(obj.color, obj.alpha)
+        gl.glLineWidth(w)
+        gl.glColor4ub(*color)
+        # FIXME: rewrite with glMatrix1fv
+        gl.glBegin(gl.GL_LINE_STRIP)
+        gl.glVertex2d(x, y)
+        gl.glVertex2d(x2, y2)
+        gl.glVertex2d(x3, y3)
+        gl.glEnd()
+
+    def _render_text(self, obj):
+        # FIXME: support scalable fonts
+        # FIXME: align text (e.g., centerling)
+        x, y = self.normalize_position(obj.x, obj.y)
+        w = self.normalize_size(8 * len(obj.text))
+        color = self.palette.rgba(obj.color, obj.alpha)
+        gl.glColor4ub(*color)
+        gl.glPushMatrix()
+        gl.glRasterPos3f(x - w / 2, y, .001)
+        for c in obj.text:
+            gl.glBitmap(8, 8, 0, 0, 8, 0, self.font_bitmap[ord(c)])
         gl.glPopMatrix()
 
     def render_objects(self, objs):
@@ -165,6 +235,7 @@ class OpenGL(SDL):
         pygame.display.flip()
         # Slowly rotate the screen.
         self.rot_z += .02
+        self.zoom *= 0.9999
 
     def _process_event(self, event):
         super()._process_event(event)
